97766cadeb275d78a2f695d3d84c08a30e91e5c5
kevin <kevin@Phantom>
2016-07-05 17:55:27 -0400

    started to generate random images for survey.

--- imageFinder/web/views.py
+++ imageFinder/web/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Image
+import random
 # Create your views here.
 def index(request):
-    
-    rand_img =  Image.objects.first()
-    return render(request, 'web/index.html')#show the homePage
+    #lets get a random number going
+    randNum = random.randint(0,Image.objects.count()-1)
+    rand_img =  Image.objects.all()[randNum]
+    context = {'randomImg' : rand_img}
+    return render(request, 'web/index.html',context)#show the homePage
 
 def search(request, term):
     return render(request, 'web/search.html')#show the search page
