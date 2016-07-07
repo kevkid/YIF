f030971b65300ba5e752403db117a20431ca3db2
kevin <kevin@Phantom>
2016-07-07 16:56:39 -0400

    Added image scanning

--- imageFinder/imageFinder/settings.py
+++ imageFinder/imageFinder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tools.apps.ToolsConfig',              
     'web.apps.WebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_IMAGES = '/web/static/web/images/'
--- imageFinder/tools/views.py
+++ imageFinder/tools/views.py
@@ -1,23 +1,42 @@
-from django.shortcuts import render
 import os
-from .models import Image, Classes
+from web.models import Image, Classes
 from django.shortcuts import get_object_or_404, render
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.conf.urls.static import static
+from imageFinder.settings import STATIC_URL, STATIC_IMAGES
+from django.contrib.staticfiles.templatetags.staticfiles import static
+import tools, web
 # Create your views here.
 def index(request):
-    #lets get a random number going
-    
+    #lets get a random number going    
     return render(request, 'tools/index.html')#show the homePage
+
+
 def ScanImages(request):
+    allImages = list(Image.objects.values_list('image_location', flat=True))
     files = []
     fileRoots = []
-    for root, directories, filenames in os.walk('/web/static/web/images/'):
+    #path = STATIC_ROOT + 'web/images/'
+    pth = web.__path__[0] + "/static/web/images"#os.path.join(tools.__path__,"static/web/images")
+    for root, directories, filenames in os.walk(pth):#probably something wrong with the location
         for filename in filenames:
-            image = get_object_or_404(Image,image_location=os.path.join(root,filename))
-            if image is None:
-                 image = Image(image)
-            files.append(os.path.join(root,filename))
-            
+            files.append("images/" + filename)#temp, will need to chance            
             fileRoots.append(root)
             
-    
+    matches = set(allImages).intersection(set(files))#get the matches
+    differenceDB_Matches = list(set(allImages) - (matches))
+    #if not in the list of files delete the image...
+    for item in differenceDB_Matches:
+        #to reduce latency
+        instance = Image.objects.get(image_location = item)
+        instance.delete()
+    #if in the file list and not in the matches add it to the db
+    differenceFiles_Matches = list(set(files) - set(matches))
+    for item in differenceFiles_Matches:
+        #to reduce latency
+        instance = Image(image_location=item)
+        instance.save()
+    return render(request, 'tools/ScanImages.html')#show the homePage
+    #return HttpResponseRedirect(reverse('tools:ScanImages', args=()))
+        
